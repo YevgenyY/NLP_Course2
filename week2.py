652e8717263e60af48d7206eb3f425e501921788
Yevgeny Y <yy@ixi.ru>
2021-06-22 14:29:59 +0300

    Make transition_matrix & emission_matrix

--- week2.py
+++ week2.py
@@ -114,3 +114,253 @@ states = sorted(tag_counts.keys())
 print(f"Number of POS tags (number of 'states'): {len(states)}")
 print("View these POS tags (states)")
 print(states)
+
+print("transition examples: ")
+for ex in list(transition_counts.items())[:3]:
+    print(ex)
+print()
+
+print("emission examples: ")
+for ex in list(emission_counts.items())[200:203]:
+    print (ex)
+print()
+
+print("ambiguous word example: ")
+for tup,cnt in emission_counts.items():
+    if tup[1] == 'back': print (tup, cnt)
+
+
+# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: predict_pos
+
+def predict_pos(prep, y, emission_counts, vocab, states):
+    '''
+    Input:
+        prep: a preprocessed version of 'y'. A list with the 'word' component of the tuples.
+        y: a corpus composed of a list of tuples where each tuple consists of (word, POS)
+        emission_counts: a dictionary where the keys are (tag,word) tuples and the value is the count
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+        states: a sorted list of all possible tags for this assignment
+    Output:
+        accuracy: Number of times you classified a word correctly
+    '''
+
+    # Initialize the number of correct predictions to zero
+    num_correct = 0
+
+    # Get the (tag, word) tuples, stored as a set
+    all_words = set(emission_counts.keys())
+
+    # Get the number of (word, POS) tuples in the corpus 'y'
+    total = len(y)
+    for word, y_tup in zip(prep, y):
+
+        # Split the (word, POS) string into a list of two items
+        y_tup_l = y_tup.split()
+
+        # Verify that y_tup contain both word and POS
+        if len(y_tup_l) == 2:
+
+            # Set the true POS label for this word
+            true_label = y_tup_l[1]
+
+        else:
+            # If the y_tup didn't contain word and POS, go to next word
+            continue
+
+        count_final = 0
+        pos_final = ''
+
+        # If the word is in the vocabulary...
+        if word in vocab:
+            for pos in states:
+
+                ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+                # define the key as the tuple containing the POS and word
+                key = (pos, word)
+
+                # check if the (pos, word) key exists in the emission_counts dictionary
+                if key in emission_counts.keys():  # complete this line
+
+                    # get the emission count of the (pos,word) tuple
+                    count = emission_counts[key]
+
+
+                    # keep track of the POS with the largest count
+                    if count > count_final:  # complete this line
+
+                        # update the final count (largest count)
+                        count_final = count
+
+                        # update the final POS
+                        pos_final = pos
+
+            # If the final POS (with the largest count) matches the true POS:
+            if pos_final == true_label:  # complete this line
+
+                # Update the number of correct predictions
+                num_correct += 1
+
+    ### END CODE HERE ###
+    accuracy = num_correct / total
+
+    return accuracy
+
+accuracy_predict_pos = predict_pos(prep, y, emission_counts, vocab, states)
+print(f"Accuracy of prediction using predict_pos is {accuracy_predict_pos:.4f}")
+
+# Part 2.1 Generating Matrices
+# Creating the 'A' transition probabilities matrix
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: create_transition_matrix
+def create_transition_matrix(alpha, tag_counts, transition_counts):
+    '''
+    Input:
+        alpha: number used for smoothing
+        tag_counts: a dictionary mapping each tag to its respective count
+        transition_counts: transition count for the previous word and tag
+    Output:
+        A: matrix of dimension (num_tags, num_tags)
+    '''
+    # Get a sorted list of unique POS tags
+    all_tags = sorted(tag_counts.keys())
+
+    # Count the number of unique POS tags
+    num_tags = len(all_tags)
+
+    # Initialize the transition matrix 'A'
+    A = np.zeros((num_tags, num_tags))
+
+    # Get the unique transition tuples (previous POS, current POS)
+    trans_keys = set(transition_counts.keys())
+
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+    # Go through each row of the transition matrix A
+    for i in range(num_tags):
+
+        # Go through each column of the transition matrix A
+        for j in range(num_tags):
+
+            # Initialize the count of the (prev POS, current POS) to zero
+            count = 0
+
+            # Define the tuple (prev POS, current POS)
+            # Get the tag at position i and tag at position j (from the all_tags list)
+            key = (all_tags[i], all_tags[j])
+
+            # Check if the (prev POS, current POS) tuple
+            # exists in the transition counts dictionary
+            if key in trans_keys:  # complete this line
+
+                # Get count from the transition_counts dictionary
+                # for the (prev POS, current POS) tuple
+                count = transition_counts[key]
+
+            # Get the count of the previous tag (index position i) from tag_counts
+            count_prev_tag = tag_counts[all_tags[i]]
+
+            # Apply smoothing using count of the tuple, alpha,
+            # count of previous tag, alpha, and total number of tags
+            A[i, j] = (count + alpha) / (count_prev_tag + alpha * num_tags)
+
+    ### END CODE HERE ###
+
+    return A
+
+alpha = 0.001
+A = create_transition_matrix(alpha, tag_counts, transition_counts)
+# Testing your function
+print(f"A at row 0, col 0: {A[0,0]:.9f}")
+print(f"A at row 3, col 1: {A[3,1]:.4f}")
+
+print("View a subset of transition matrix A")
+A_sub = pd.DataFrame(A[30:35,30:35], index=states[30:35], columns = states[30:35] )
+print(A_sub)
+
+# Create the 'B' emission probabilities matrix
+# UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: create_emission_matrix
+
+def create_emission_matrix(alpha, tag_counts, emission_counts, vocab):
+    '''
+    Input:
+        alpha: tuning parameter used in smoothing
+        tag_counts: a dictionary mapping each tag to its respective count
+        emission_counts: a dictionary where the keys are (tag, word) and the values are the counts
+        vocab: a dictionary where keys are words in vocabulary and value is an index.
+               within the function it'll be treated as a list
+    Output:
+        B: a matrix of dimension (num_tags, len(vocab))
+    '''
+
+    # get the number of POS tag
+    num_tags = len(tag_counts)
+
+    # Get a list of all POS tags
+    all_tags = sorted(tag_counts.keys())
+
+    # Get the total number of unique words in the vocabulary
+    num_words = len(vocab)
+
+    # Initialize the emission matrix B with places for
+    # tags in the rows and words in the columns
+    B = np.zeros((num_tags, num_words))
+
+    # Get a set of all (POS, word) tuples
+    # from the keys of the emission_counts dictionary
+    emis_keys = set(list(emission_counts.keys()))
+
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+    # Go through each row (POS tags)
+    for i in range(num_tags):  # complete this line
+
+        # Go through each column (words)
+        for j in range(num_words):  # complete this line
+
+            # Initialize the emission count for the (POS tag, word) to zero
+            count = 0
+
+            # Define the (POS tag, word) tuple for this row and column
+            key = (all_tags[i], vocab[j])
+
+            # check if the (POS tag, word) tuple exists as a key in emission counts
+            if key in emis_keys:  # complete this line
+
+                # Get the count of (POS tag, word) from the emission_counts d
+                count = emission_counts[key]
+
+            # Get the count of the POS tag
+            count_tag = tag_counts[key[0]]
+
+            # Apply smoothing and store the smoothed value
+            # into the emission matrix B for this row and column
+            B[i, j] = (count + alpha) / (count_tag + alpha*num_words)
+
+    ### END CODE HERE ###
+    return B
+
+# creating your emission probability matrix. this takes a few minutes to run.
+B = create_emission_matrix(alpha, tag_counts, emission_counts, list(vocab))
+
+print(f"View Matrix position at row 0, column 0: {B[0,0]:.9f}")
+print(f"View Matrix position at row 3, column 1: {B[3,1]:.9f}")
+
+# Try viewing emissions for a few words in a sample dataframe
+cidx  = ['725','adroitly','engineers', 'promoted', 'synergy']
+
+# Get the integer ID for each word
+cols = [vocab[a] for a in cidx]
+
+# Choose POS tags to show in a sample dataframe
+rvals =['CD','NN','NNS', 'VB','RB','RP']
+
+# For each POS tag, get the row number from the 'states' list
+rows = [states.index(a) for a in rvals]
+
+# Get the emissions for the sample of words, and the sample of POS tags
+B_sub = pd.DataFrame(B[np.ix_(rows,cols)], index=rvals, columns = cidx )
+print(B_sub)
