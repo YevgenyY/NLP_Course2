ea3b83204cfa4d27a75340f4e320f83cf4835174
Yevgeny Y <yy@ixi.ru>
2021-06-26 13:17:32 +0300

    Finalize week 2

--- week2.py
+++ week2.py
@@ -577,3 +577,47 @@ pred = viterbi_backward(best_probs, best_paths, prep, states)
 m=len(pred)
 print('The prediction for pred[-7:m-1] is: \n', prep[-7:m-1], "\n", pred[-7:m-1], "\n")
 print('The prediction for pred[0:8] is: \n', pred[0:7], "\n", prep[0:7])
+
+
+# UNQ_C8 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: compute_accuracy
+def compute_accuracy(pred, y):
+    '''
+    Input:
+        pred: a list of the predicted parts-of-speech
+        y: a list of lines where each word is separated by a '\t' (i.e. word \t tag)
+    Output:
+
+    '''
+    num_correct = 0
+    total = 0
+
+    # Zip together the prediction and the labels
+    for prediction, y in zip(pred, y):
+
+        ### START CODE HERE (Replace instances of 'None' with your code) ###
+        # Split the label into the word and the POS tag
+        word_tag_tuple = y.split()
+
+        # Check that there is actually a word and a tag
+        # no more and no less than 2 items
+        if len(word_tag_tuple) != 2:  # complete this line
+            continue
+
+            # store the word and tag separately
+        word, tag = word_tag_tuple
+
+        # Check if the POS tag label matches the prediction
+        if tag == prediction:  # complete this line
+
+            # count the number of times that the prediction
+            # and label match
+            num_correct += 1
+
+        # keep track of the total number of examples (that have valid labels)
+        total += 1
+
+        ### END CODE HERE ###
+    return num_correct / total
+
+print(f"Accuracy of the Viterbi algorithm is {compute_accuracy(pred, y):.4f}")
